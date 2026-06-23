ae8398930edb6fb9f2f6337489515032fc272fb4
hoonjo123 <johoon030@gmail.com>
2026-06-23 14:39:35 +0900

    feat: Comment CUD 작성, Service test 단위테스트

---
+++ service/comment/src/main/java/joney/board/comment/service/CommentService.java
@@ -0,0 +1,80 @@
+package joney.board.comment.service;
+
+import joney.board.comment.entity.Comment;
+import joney.board.comment.repository.CommentRepository;
+import joney.board.comment.service.request.CommentCreateRequest;
+import joney.board.comment.service.response.CommentResponse;
+import joney.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static java.util.function.Predicate.not;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final Snowflake snowflake = new Snowflake();
+
+
+    @Transactional
+    public CommentResponse create(CommentCreateRequest request) {
+        Comment parent = findParent(request);
+        Comment comment = commentRepository.save(
+                Comment.create(
+                        snowflake.nextId(),
+                        request.getContent(),
+                        parent == null ? null : parent.getCommentId(),
+                        request.getArticleId(),
+                        request.getWriterId()
+                )
+        );
+        return CommentResponse.from(comment);
+    }
+
+    private Comment findParent(CommentCreateRequest request) {
+        Long parentCommentId = request.getParentCommentId();
+        if ( parentCommentId == null) {
+            return null;
+        }
+        return commentRepository.findById(parentCommentId)
+                .filter(not(Comment::getDeleted))
+                .filter(Comment::isRoot)
+                .orElseThrow();
+    }
+
+    public CommentResponse read(Long commentId) {
+        return CommentResponse.from(
+                commentRepository.findById(commentId).orElseThrow()
+        );
+    }
+
+    @Transactional
+    public void delete(Long commentId) {
+        commentRepository.findById(commentId)
+                .filter(not(Comment::getDeleted))
+                .ifPresent(comment -> {
+                    if (hasChildren(comment)) {
+                        comment.delete();
+                    } else {
+                        delete(comment);
+                    }
+                });
+    }
+
+    // 최대 2뎁스이므로 자식이 있는지 확인할 때 2개만 조회하면 된다.
+    private boolean hasChildren(Comment comment) {
+        return commentRepository.countBy(comment.getArticleId(), comment.getCommentId(), 2L) == 2;
+    }
+
+    private void delete(Comment comment) {
+        commentRepository.delete(comment);
+        if (!comment.isRoot()) {
+            commentRepository.findById(comment.getParentCommentId())
+                    .filter(Comment::getDeleted)
+                    .filter(not(this::hasChildren))
+                    .ifPresent(this::delete);
+        }
+    }
+}
